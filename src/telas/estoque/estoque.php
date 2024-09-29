7b990ee34ba4d7ffd501941429ddb1c3b3591d48
acaf02 <anacarol.alves1@outlook.com>
2024-09-29 16:33:48 -0300

    Add: tabela com os insumos e div com botões de gerenciamento

--- src/telas/estoque/estoque.php
+++ src/telas/estoque/estoque.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="inicio.css">
+    <link rel="stylesheet" href="estoque.css">
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -13,12 +13,7 @@
     <!-- Font Awesome-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <style>
-        .card-custom {
-            width: 260px;
-            margin: 10px;
-        }
-    </style>
+   
 
 </head>
 
@@ -28,6 +23,65 @@
 include_once('../../componentes/header.php');
 include_once('../../componentes/navbar.php');
 ?>
+
+<div class="container" style="padding:20px;">
+        <?php
+        // Verifica se há uma mensagem para exibir
+        if (isset($_GET['msg'])) {
+            $msg = $_GET['msg'];
+            echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    ' . $msg . '
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>';
+        }
+        ?>
+       
+       <div class="container">
+        <div class="d-flex justify-content-end align-items-center mb-3">
+            <a href="../gerenciar/gerenciar.php" class="btn btn-dark" style="padding: 10px;">Gerenciar</a>
+            <a href="../cadastro/cadastro.php" class="btn btn-dark mx-2" style="padding: 10px;">
+                <i class="fa-solid fa-circle-plus"></i> Cadastrar
+            </a>
+            <div class="search-container" style="padding: 5px;">
+                <input type="text" placeholder="Pesquisar..." class="form-control search-input">
+            </div>
+            <button class="btn btn-dark search-button" ">
+                    <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
+                </button>
+            <i class="fa fa-sliders mx-2" style="font-size:30px; padding:6px;"></i>
+        </div>
+    </div>
+        <table class="table table-hover text-center">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">Produto</th>
+                    <th scope="col">Peso/Unidade</th>
+                    <th scope="col">Quantidade</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // Conexão com o banco de dados
+                include "../../db/db_connection.php";
+
+                // Query para selecionar todos os registros
+                $sql = "SELECT * FROM `insumo`";
+                $result = mysqli_query($connection, $sql);
+
+                // Loop para exibir os dados em uma tabela
+                while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                    <tr>
+                        <td><?php echo $row['produto'] ?></td>
+                        <td><?php echo $row['peso'] . '' . $row['unidade']; ?></td>
+                        <td><?php echo $row['quantidade'] ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
   
     <!-- Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
