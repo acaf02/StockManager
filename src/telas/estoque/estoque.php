aee858be9fc0081c3e00d702a25b70a399e1e146
acaf02 <anacarol.alves1@outlook.com>
2024-10-10 17:00:20 -0300

    Update: div de pesquisa e botões

--- src/telas/estoque/estoque.php
+++ src/telas/estoque/estoque.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -6,25 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estoque.css">
-
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <!-- Font Awesome-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-   
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 
 <body>
 
-<?php
-include_once('../../componentes/header.php');
-include_once('../../componentes/navbar.php');
-?>
+    <?php
+    include_once('../../componentes/header.php');
+    include_once('../../componentes/navbar.php');
+    ?>
 
-<div class="container" style="padding:20px;">
+    <div class="container" style="padding:20px;">
         <?php
         // Verifica se há uma mensagem para exibir
         if (isset($_GET['msg'])) {
@@ -35,22 +27,21 @@ include_once('../../componentes/navbar.php');
                   </div>';
         }
         ?>
-       
-       <div class="container">
+
         <div class="d-flex justify-content-end align-items-center mb-3">
-            <a href="../gerenciar/gerenciar.php" class="btn btn-dark" style="padding: 10px;">Gerenciar</a>
-            <a href="../cadastro/cadastro.php" class="btn btn-dark mx-2" style="padding: 10px;">
+            <a href="../gerenciar/gerenciar.php" class="btn btn-dark">Gerenciar</a>
+            <a href="../cadastro/cadastro.php" class="btn btn-dark mx-2">
                 <i class="fa-solid fa-circle-plus"></i> Cadastrar
             </a>
-            <div class="search-container" style="padding: 5px;">
-                <input type="text" placeholder="Pesquisar..." class="form-control search-input">
+            <div class="search-container">
+                <input type="text" class="form-control" placeholder="Pesquisar...">
             </div>
-            <button class="btn btn-dark search-button" ">
-                    <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
-                </button>
+            <button class="btn btn-dark">
+                <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
+            </button>
             <i class="fa fa-sliders mx-2" style="font-size:30px; padding:6px;"></i>
         </div>
-    </div>
+
         <table class="table table-hover text-center">
             <thead class="table-dark">
                 <tr>
@@ -70,21 +61,20 @@ include_once('../../componentes/navbar.php');
 
                 // Loop para exibir os dados em uma tabela
                 while ($row = mysqli_fetch_assoc($result)) {
-                ?>
+                    ?>
                     <tr>
                         <td><?php echo $row['produto'] ?></td>
-                        <td><?php echo $row['peso'] . '' . $row['unidade']; ?></td>
+                        <td><?php echo $row['peso'] . ' ' . $row['unidade']; ?></td>
                         <td><?php echo $row['quantidade'] ?></td>
                     </tr>
-                <?php
+                    <?php
                 }
                 ?>
             </tbody>
         </table>
     </div>
-  
-    <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
