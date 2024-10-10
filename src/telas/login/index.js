9bc04f39889ec60e323f683bc56940caf6b1f511
acaf02 <anacarol.alves1@outlook.com>
2024-10-10 16:58:16 -0300

    Update: adicionado codigo do icone para mostar e ocultar senha

--- src/telas/login/index.js
+++ src/telas/login/index.js
@@ -1,33 +1,49 @@
-$(document).ready(function() {
-    $("#login").on('click', function() {
-        var username = $("#username").val();
-        var password = $("#password").val();
+$(document).ready(function () {
+  $("#login").on("click", function () {
+    var username = $("#username").val();
+    var password = $("#password").val();
 
-        if (username === "" || password === "") {
-            alert("Erro em login ou senha");
-        } else {
-            $.ajax({
-                url: 'index.php',
-                method: 'POST',
-                data: {
-                    login: 1,
-                    username: username,
-                    password: password
-                },
-                success: function(response) {
-                    response = response.trim();
-                    if (response === 'success') {
-                        alert("Login bem-sucedido");
-                        window.location.href = "../inicio/inicio.php";
-                    } else {
-                        alert(response); 
-                    }
-                },
-                error: function(xhr, status, error) {
-                    console.error("AJAX Error: " + status + " - " + error);
-                },
-                dataType: 'text'
-            });
-        }
-    });
+    if (username === "" || password === "") {
+      alert("Erro em login ou senha");
+    } else {
+      $.ajax({
+        url: "index.php",
+        method: "POST",
+        data: {
+          login: 1,
+          username: username,
+          password: password,
+        },
+        success: function (response) {
+          response = response.trim();
+          if (response === "success") {
+            alert("Login bem-sucedido");
+            window.location.href = "../inicio/inicio.php";
+          } else {
+            alert(response);
+          }
+        },
+        error: function (xhr, status, error) {
+          console.error("AJAX Error: " + status + " - " + error);
+        },
+        dataType: "text",
+      });
+    }
+  });
 });
+
+
+
+//funcção para icone de mostrar e esconder senha
+const password = document.getElementById("password");
+        const icone = document.getElementById("icones").children[0]; // Pega o <img> dentro do #icones
+
+        function showHide() {
+            if (password.type === "password") {
+                password.setAttribute("type", "text");
+                icone.src = "../../assets/imagens/hide.png"; // Muda para ícone de esconder
+            } else {
+                password.setAttribute("type", "password");
+                icone.src = "../../assets/imagens/show.png"; // Muda para ícone de mostrar
+            }
+        }
--- src/telas/login/index.php
+++ src/telas/login/index.php
@@ -17,11 +17,11 @@ if (isset($_POST['login'])) {
         die("Connection failed: " . $connection->connect_error);
     }
 
-    $username = $connection->real_escape_string($_POST['username']); // Escapa caracteres especiais para evitar SQL Injection
+    $username = $connection->real_escape_string($_POST['username']);
     $senha = $_POST['password']; // Obtém a senha
 
     // Consulta SQL para selecionar o usuário
-    $query = "SELECT senha FROM login WHERE login_funcionario = ?"; 
+    $query = "SELECT senha FROM login WHERE login_funcionario = ?";
     $stmt = $connection->prepare($query);
 
     if (!$stmt) {
@@ -29,29 +29,29 @@ if (isset($_POST['login'])) {
     }
 
     //executa a consulta
-    $stmt->bind_param("s", $username); 
-    $stmt->execute(); 
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
     $result = $stmt->get_result();
 
     //obtém os dados do usuario
-    if ($result->num_rows > 0) { 
-        $row = $result->fetch_assoc(); 
-        $hashed_password = $row['senha']; 
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $hashed_password = $row['senha'];
 
         //Login
-        if (password_verify($senha, $hashed_password)) { 
-            $_SESSION['loggedIn'] = '1'; 
-            $_SESSION['username'] = $username; 
+        if (password_verify($senha, $hashed_password)) {
+            $_SESSION['loggedIn'] = '1';
+            $_SESSION['username'] = $username;
             exit('success');
         } else {
-            exit('Senha incorreta!'); // Retorna uma mensagem específica se a senha não corresponder
+            exit('Senha incorreta!');
         }
     } else {
-        exit('Usuário não encontrado!'); // Retorna uma mensagem específica se o usuário não for encontrado
+        exit('Usuário não encontrado!');
     }
 
-    $stmt->close(); // Fecha a declaração preparada
-    $connection->close(); // Fecha a conexão com o banco de dados
+    $stmt->close();
+    $connection->close();
 }
 ?>
 
@@ -62,16 +62,22 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
+
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 
 <body>
     <div class="login-box">
         <center>
-            <img src="../../assets/imagens/logoSM.png" alt="Logotipo" style="width: 150px; height: 80px; padding-bottom:30px;">
+            <img src="../../assets/imagens/logoSM.png" alt="Logotipo"
+                style="width: 150px; height: 80px; padding-bottom:30px;">
         </center>
         <form id="loginForm">
             <div class="user-box">
@@ -80,6 +86,9 @@ if (isset($_POST['login'])) {
             </div>
             <div class="user-box">
                 <input type="password" id="password" name="password" required>
+                <div id="icones" onclick="showHide()">
+                    <img src="../../assets/imagens/show.png" alt="Mostrar Senha" style="width: 20px; height: 20px;">
+                </div>
                 <label for="password">Senha:</label>
             </div>
             <center>
@@ -87,10 +96,15 @@ if (isset($_POST['login'])) {
             </center>
         </form>
     </div>
+
+  
+
     <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- JQuery -->
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
     <script src="index.js"></script>
 </body>
