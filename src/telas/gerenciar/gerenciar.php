a2fb5513ae26475c61392e4f677081985af06b63
acaf02 <anacarol.alves1@outlook.com>
2024-10-18 00:05:29 -0300

    update

--- src/telas/gerenciar/gerenciar.php
+++ src/telas/gerenciar/gerenciar.php
@@ -1,25 +1,3 @@
-<?php
-include "../../db/db_connection.php";
-
-
-// Verifica se há pesquisa e cria a consulta apropriada
-if (!empty($_GET['pesquisar'])) {
-    $data = $_GET['pesquisar'];
-    $data = mysqli_real_escape_string($connection, $data);
-    $sql = "SELECT * FROM insumo WHERE produto LIKE '%$data%' ORDER BY cod_insumo ASC";
-} else {
-    $sql = "SELECT * FROM insumo ORDER BY cod_insumo ASC";
-}
-
-// Executar a consulta
-$result = mysqli_query($connection, $sql);
-
-// Verifica se a consulta foi bem-sucedida
-if (!$result) {
-    die("Erro na consulta: " . mysqli_error($connection));
-}
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -28,13 +6,33 @@ if (!$result) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="gerenciar.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 
 <body>
 
     <?php
+    include "../../db/db_connection.php";
+
+    // Verifica se há pesquisa e cria a consulta apropriada
+    if (!empty($_GET['pesquisar'])) {
+        $data = $_GET['pesquisar'];
+        $data = mysqli_real_escape_string($connection, $data);
+        $sql = "SELECT * FROM insumo WHERE produto LIKE '%$data%' ORDER BY cod_insumo ASC";
+    } else {
+        $sql = "SELECT * FROM insumo ORDER BY cod_insumo ASC";
+    }
+
+    // Executar a consulta
+    $result = mysqli_query($connection, $sql);
+
+    // Verifica se a consulta foi bem-sucedida
+    if (!$result) {
+        die("Erro na consulta: " . mysqli_error($connection));
+    }
+
     include_once('../../componentes/header.php');
     include_once('../../componentes/navbar.php');
     include_once('modals/adicionar.php');
@@ -47,12 +45,15 @@ if (!$result) {
             <a href="../cadastro/cadastro.php" class="btn btn-dark mx-2">
                 <i class="fa-solid fa-circle-plus"></i> Cadastrar
             </a>
-            <div class="box-search">
-                <input type="search" class="form-control" placeholder="Pesquisar" id="pesquisar">
-            </div>
-            <button onclick="pesquisarDados()" class="btn btn-dark" style="height: 38px;">
-                <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
-            </button>
+
+            <!-- Formulário para pesquisa com método GET -->
+            <form method="GET" action="gerenciar.php" class="d-flex align-items-center">
+                <input type="search" name="pesquisar" class="form-control" placeholder="Pesquisar" id="pesquisar"
+                    value="<?php echo isset($_GET['pesquisar']) ? htmlspecialchars($_GET['pesquisar']) : ''; ?>">
+                <button type="submit" class="btn btn-dark" style="height: 38px;">
+                    <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
+                </button>
+            </form>
             <i class="fa fa-sliders mx-2" style="font-size:30px; padding:6px;"></i>
         </div>
 
@@ -68,25 +69,35 @@ if (!$result) {
                 </tr>
             </thead>
             <tbody>
-                <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-                    <tr>
-                        <td><?php echo htmlspecialchars($row['produto']); ?></td>
-                        <td><?php echo htmlspecialchars($row['peso'] . ' ' . $row['unidade']); ?></td>
-                        <td><?php echo htmlspecialchars($row['quantidade']); ?></td>
-                        <td>
-                            <!-- Botão para abrir o modal de adicionar insumos -->
-                            <a href="#" class="open-modal-adicionar" data-cod_insumo="<?php echo $row['cod_insumo']; ?>">
-                                <i class="fa-regular fa-square-plus" style="color: green; font-size:20px;"></i>
-                            </a>
-                        </td>
-                        <td>
-                            <!-- Botão para abrir o modal de retirar insumos -->
-                            <a href="#" class="open-modal-retirar" data-cod_insumo="<?php echo $row['cod_insumo']; ?>">
-                                <i class="fa-regular fa-square-minus" style="color: red; font-size:20px;"></i>
-                            </a>
-                        </td>
-                    </tr>
-                <?php } ?>
+                <?php
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($row['produto']); ?></td>
+                            <td><?php echo htmlspecialchars($row['peso'] . ' ' . $row['unidade']); ?></td>
+                            <td><?php echo htmlspecialchars($row['quantidade']); ?></td>
+                            <td>
+                                <!-- Botão para abrir o modal de adicionar insumos -->
+                                <a href="javascript:void(0)" class="open-modal-adicionar"
+                                    data-cod_insumo="<?php echo $row['cod_insumo']; ?>">
+                                    <i class="fa-regular fa-square-plus" style="color: green; font-size:20px;"></i>
+                                </a>
+                            </td>
+                            <td>
+                                <!-- Botão para abrir o modal de retirar insumos -->
+                                <a href="javascript:void(0)" class="open-modal-retirar"
+                                    data-cod_insumo="<?php echo $row['cod_insumo']; ?>">
+                                    <i class="fa-regular fa-square-minus" style="color: red; font-size:20px;"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                } else {
+                    echo "<tr><td colspan='5'>Nenhum insumo encontrado</td></tr>";
+                }
+                ?>
             </tbody>
         </table>
     </div>
@@ -95,20 +106,16 @@ if (!$result) {
     <script src="gerenciar.js"></script>
 
     <script>
+        // Evento para capturar a pesquisa via Enter
         var pesquisa = document.getElementById('pesquisar');
-
         pesquisa.addEventListener("keydown", function (event) {
             if (event.key === "Enter") {
-                pesquisarDados();
+                pesquisa.form.submit();
             }
         });
 
-        function pesquisarDados() {
-            var query = pesquisa.value;
-            window.location = 'gerenciar.php?pesquisar=' + encodeURIComponent(query);
-        }
-    </script>
 
+    </script>
 
 </body>
 
