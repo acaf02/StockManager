56c8b7cca58805df1ee4a68d02e90b38f1013b53
acaf02 <anacarol.alves1@outlook.com>
2024-10-20 19:10:46 -0300

    update: modifcado tamanhos e posição dos cards

--- src/telas/relatorio/relatorios.php
+++ src/telas/relatorio/relatorios.php
@@ -14,33 +14,36 @@ if (!isset($_SESSION['loggedIn'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="relatorio.css">
+    <title>Relatórios</title>
 
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- Font Awesome-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <style>
         .card-custom {
-    width: 300px;
-    margin: 0 20px 20px 20px; 
-}
-
+            width: 300px;
+            margin: 0 20px 20px 20px;
+        }
     </style>
 
 </head>
 
 <body>
 
-<?php
-include_once('../../componentes/header.php');
-include_once('../../componentes/navbar.php');
-?>
+    <?php
+    include_once('../../componentes/header.php');
+    include_once('../../componentes/navbar.php');
+    ?>
 
-<h1 class="title">Relatórios</h1>
+    <h1 class="title">Relatórios</h1>
 
-<!-- Cards de relatorios disponiveis--> 
+    <!-- Cards de relatorios disponiveis-->
     <div class="container">
         <div class="row justify-content-center cards-row" style="padding-top:25px;">
             <div class="card card-custom card-inventario">
@@ -55,7 +58,7 @@ include_once('../../componentes/navbar.php');
 
             <div class="card card-custom card-estatisticas">
                 <div class="card-body">
-                <img class="card-img-top" src="../../assets/imagens/relatorio.png" alt="Relatórios">
+                    <img class="card-img-top" src="../../assets/imagens/relatorio.png" alt="Relatórios">
                     <h5 class="card-title">Estatísticas</h5>
                     <p class="card-text">Este relatório mostra quais são os itens mais e menos consumidos.</p>
                     <a href="livros/livros.php" class="btn btn-primary">Abrir</a>
@@ -64,9 +67,11 @@ include_once('../../componentes/navbar.php');
             </div>
         </div>
     </div>
-    
+
     <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
