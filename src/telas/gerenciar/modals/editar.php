ebcc5f6f5b01e2e21400abe5469f296ba931ee96
acaf02 <anacarol.alves1@outlook.com>
2024-12-14 15:20:20 -0300

    feat: alteração do titulo do modal

--- src/telas/gerenciar/modals/editar.php
+++ src/telas/gerenciar/modals/editar.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalEditarLabel">Editar Estoque</h5>
+                <h5 class="modal-title" id="modalEditarLabel">Editar Insumo</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
